580d8f7a39ef125e52120e8576f5800ae12a5afe
Antoine Amacher <aamacher@inverse.ca>
2016-01-27 15:09:23 -0500

    Formatting filter check

--- lib/pf/Switch/Meraki/AP_http_V2.pm
+++ lib/pf/Switch/Meraki/AP_http_V2.pm
@@ -142,7 +142,7 @@ sub returnRadiusAccessAccept {
 
     my $filter = pf::access_filter::radius->new;
     my $rule = $filter->test('returnRadiusAccessAccept', $args);
-    $radius_reply_ref = $filter->handleAnswerInRule($rule,$args,$radius_reply_ref);
+    ($radius_reply_ref, $status) = $filter->handleAnswerInRule($rule,$args,$radius_reply_ref);
 
     return [$RADIUS::RLM_MODULE_OK, %$radius_reply_ref];
 }
